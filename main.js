b8d080a8f586a52fbabb2fca93ded524b97c1d0e
Daniel Ruoso <daniel@ruoso.com>
2016-06-20 09:39:01 -0400

    fix syntax issues

--- main.js
+++ main.js
@@ -4,39 +4,11 @@ var Output        = require("./lib/Output.js");
 var SnakeWorld    = require("./lib/SnakeWorld.js");
 var Snake         = require("./lib/Snake.js");
 
-var i = 200;
-var j = 50
+
+var i = 100;
+var j = 40
 var m = new Monitor(i,j);
+var s = new Snake(i,j,Neuron,Output,m);
+
 setInterval(function() { m.output() }, 20);
 
-// let's make a row of j outputs
-var outputs = [];
-for (var jj = 0; jj < j; jj++) {
-    outputs.push(new Output(jj, m));
-};
-
-// let's make rows of neurons in a chain
-var current_output = outputs;
-var a = 2;
-for (var ii = 1; ii < i; ii++) {
-    // let's make each row with neurons, and connect each neuron in a
-    // row with every neuron in the next row.
-    var new_output = [];
-    for (var jj = 0; jj < j; jj++) {
-	var part_of_output = [];
-	for (var jjj = (jj-a<0?0:jj-a);
-	     ((jjj <= (jj+a)) && (jjj < j));
-	     jjj++) {
-	    part_of_output.push(current_output[jjj]);
-	}
-	new_output.push(new Neuron(ii, jj, part_of_output, m));
-    }
-    current_output = new_output;
-}
-
-// the inputs are the base of the chain
-var inputs = current_output;
-setInterval(function() {
-    var x = Math.floor(Math.random() * j);
-    inputs[x].message();
-}, 10);

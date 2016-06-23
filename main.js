e79c2bfb88808999bc72a785b5859d6fe4733590
Daniel Ruoso <daniel@ruoso.com>
2016-06-23 00:23:54 -0400

    new framework completed

--- main.js
+++ main.js
@@ -1,21 +1,40 @@
 var Monitor       = require("./lib/Monitor.js");
 var Neuron        = require("./lib/Neuron.js");
 var Output        = require("./lib/Output.js");
-var SnakeWorld    = require("./lib/SnakeWorld.js");
 var Snake         = require("./lib/Snake.js");
-var SnakePosition = require("./lib/SnakePosition.js");
+var Rules         = require("./lib/Rules.js");
 
 var brain_r = 120;
 var brain_c = 40;
-var map_w = 40;
-var map_h = 130;
+var map_w = 130;
+var map_h = 40;
 
-var s = new Snake(brain_r, brain_c, Neuron, Output);
-var w = new SnakeWorld(map_w, map_h);
-var p = new SnakePosition(w, s);
-var m = new Monitor(brain_r, brain_c, w, [ p ]);
-m.start();
+var snake = new Snake(brain_r, brain_c, Neuron, Output);
+var walls =
+    [
+	{ start: { x: 0, y: 0},
+	  end:   { x: 129, y: 0},
+	},
+	{ start: { x: 0, y: 0},
+	  end:   { x: 0, y: 39},
+	},
+	{ start: { x: 129, y: 0},
+	  end:   { x: 129, y: 39},
+	},
+	{ start: { x: 0, y: 39},
+	  end:   { x: 129, y: 39},
+	},
+    ];
+var food =
+    [
+	{ x: 15, y: 10 },
+	{ x: 50, y: 25 },
+	{ x: 75, y: 35 },
+    ];
+var rules = new Rules(map_h, map_w, snake, walls, food);
+var monitor = new Monitor(brain_r, brain_c, rules);
+monitor.start();
 setInterval(function() {
-    m.output();
+    monitor.output();
 }, 40);
 
